7e28f5ec5a943ba78455ff15339760f8b94a86ce
talta <t.taltallc@gmail.com>
2017-04-06 19:28:20 -0400

    updated tests for the baord comopnent

--- src/components/__tests__/game.test.js
+++ src/components/__tests__/game.test.js
@@ -1,9 +1,5 @@
 import React from 'react';
-
-import Header from './header';
-import GuessSection from './guess-section';
-import GuessCount  from './guess-count';
-import GuessList from './guess-list';
+import {shallow} from 'enzyme';
 
 
 describe('<Board />', ()=>{
